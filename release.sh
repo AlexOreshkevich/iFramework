ce7a743f3da7a848eec48319ff16e678454fafc4
alex oreshkevich <alex.oreshkevich@redsoft.pro>
2013-05-22 18:40:32 +0300

    release-setting

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git reset
+git tag -d $1
+git push origin :refs/tags/$1
 git pull origin development
 mvn --batch-mode -e release:clean release:prepare -DreleaseVersion=$1 -DdevelopmentVersion=$2
 mvn --batch-mode -e release:perform
