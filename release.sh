5a2216a04c17601f9627244b26f29c0276fa55f5
alex oreshkevich <alex.oreshkevich@redsoft.pro>
2013-05-22 19:04:13 +0300

    fix script

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 git tag -d $1
 git push origin :refs/tags/$1
 git pull origin development
-mvn --batch-mode -e release:clean release:prepare -DreleaseVersion=$1 -DdevelopmentVersion=$2
-mvn --batch-mode -e release:perform
+mvn --batch-mode -e -X release:clean release:prepare -DreleaseVersion=$1 -DdevelopmentVersion=$2
+mvn -e release:perform
