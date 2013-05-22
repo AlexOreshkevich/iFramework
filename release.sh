3eb9c8461dd6eacd7606b7f96d97d5f820cb66bc
alex oreshkevich <alex.oreshkevich@redsoft.pro>
2013-05-22 19:24:01 +0300

    test

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-git tag -d $1
-git push origin :refs/tags/$1
-git pull origin development
-mvn --batch-mode -e -X release:clean release:prepare -DreleaseVersion=$1 -DdevelopmentVersion=$2
+git tag -d iFramework-$1
+git push origin :refs/tags/iFramework-$1
+mvn --batch-mode -e -X release:prepare -DreleaseVersion=$1 -DdevelopmentVersion=$2
 mvn -e release:perform
