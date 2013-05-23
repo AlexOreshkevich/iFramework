fc272d060d47130c6b9078b0182e84a55c2103b9
alex oreshkevich <alex.oreshkevich@redsoft.pro>
2013-05-23 15:16:14 +0300

    Remove jaxb-ri dependency (moving to standard jaxb implementation)

--- exjaxb/src/main/java/by/neosoft/iframework/exjaxb/package-info.java
+++
@@ -1,1 +0,0 @@
-package by.neosoft.iframework.exjaxb;
--- release.sh
+++ release.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-git tag -d iFramework-$1
-git push origin :refs/tags/iFramework-$1
-mvn --batch-mode -e -X release:prepare -DreleaseVersion=$1 -DdevelopmentVersion=$2
-mvn -e release:perform
+mvn --batch-mode -e release:prepare -Dgithub.username=$1 -Dgithub.password=$2
+mvn --batch-mode -e release:perform
