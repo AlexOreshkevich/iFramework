cc3057c3c735a5ff994a3e764d8a73340aa9fa40
alex oreshkevich <alex.oreshkevich@redsoft.pro>
2013-07-10 13:02:35 +0300

    missing files

---
+++ iframework-core/gwtpx/src/main/java/pro/redsoft/iframework/client/provider/FactoryLoader.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2013 REDSOFT.PRO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package pro.redsoft.iframework.client.provider;
+
+import pro.redsoft.iframework.client.factory.AbstractTabFactory;
+import pro.redsoft.iframework.client.presenter.AbstractTabPresenter;
+import pro.redsoft.iframework.client.view.AbstractTabView;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * FactoryLoader.
+ * 
+ * @author Alex N. Oreshkevich
+ */
+public interface FactoryLoader {
+
+  /**
+   * @param iTabType
+   * @param asyncCallback
+   */
+      void
+      get(ITabType iTabType,
+          AsyncCallback<AbstractTabFactory<? extends AbstractTabView, ? extends AbstractTabPresenter<?>>> asyncCallback);
+}
